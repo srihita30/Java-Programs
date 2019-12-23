0bf9d0f65cafcfb0db0d42ca33a696574f450a36
Srihita Sai Ayinala <58342544+srihita30@users.noreply.github.com>
2019-12-23 19:01:51 +0530

    This code will also add 5 numbers. It will be using its

---
+++ ExtendAddFunctionality.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; 
+
+public class ExtendAddFunctionality{
+	public static void main (String[] args){
+
+		Scanner obj = new Scanner(System.in);
+
+		System.out.println("Enter any 5 numbers to add ");
+
+		int num1 = obj.nextInt();
+
+		int num2 = obj.nextInt();
+
+		int num3 = obj.nextInt();
+
+		int num4 = obj.nextInt();
+		
+		int num5 = obj.nextInt(); 
+
+		int newArray[] = {num1, num2, num3, num4, num5};
+
+		AddNumbers an = new AddNumbers(); 
+
+		int addingTheNumbers = an.addingTheNumbers(newArray);
+
+		System.out.println("The sum of all the 5 numbers entered is " + addingTheNumbers);
+
+	}
+}
