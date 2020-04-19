7eafab0aed2ab1a78302ab6c57dc4ebf2d28ac38
Srihita Sai Ayinala <58342544+srihita30@users.noreply.github.com>
2020-04-19 14:54:03 +0530

    Get - Set class for tomorrows RandomValue class.

---
+++ Participant.java
@@ -0,0 +1,23 @@
+public class Participant{
+	
+	private int theValue = 0;
+
+	private String nameOfGuy = " ";
+
+	public int getTheValue(){
+		return this.theValue;
+	}
+
+	public String getNameOfGuy(){
+		return this.nameOfGuy;
+	}
+
+	public void setTheValue(int val){
+		this.theValue = val;
+	}
+
+	public void setNameOfGuy(String boyName){
+		this.nameOfGuy = boyName;
+	}
+
+}
