ec4aedb308aa371bb9dc290ff24a3bafca26a25e
Srihita Sai Ayinala <58342544+srihita30@users.noreply.github.com>
2020-04-28 23:36:04 +0530

    The get set class for the "EmployeeDepartment" class.

---
+++ Employee.java
@@ -0,0 +1,47 @@
+public class Employee{
+	
+	private int id = 0;
+
+	private String nameOfPerson = " ";
+
+	private int salary = 0; 
+
+	private String department = " "; 
+
+	public int getId(){
+		return this.id;
+	}
+
+	public String getNameOfPerson(){
+		return this.nameOfPerson;
+	}
+
+	public String getDepartment(){
+		return this.department;
+	}
+
+	public int getSalary(){
+		return this.salary;
+	}
+
+	public void setId(int someID){
+
+			this.id = someID;
+	}
+
+	public void setNameOfPerson(String personName){
+		this.nameOfPerson = personName;
+	}
+
+
+	public void setDepartment(String departmentName){
+		this.department = departmentName;
+	}
+
+	public void setSalary(int personSalary){
+
+			
+			this.salary = personSalary;
+		
+	}
+}
