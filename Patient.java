b5d7bd434a1dd26578acec3bdf55dbf247ab7555
Srihita Sai Ayinala <58342544+srihita30@users.noreply.github.com>
2020-05-15 22:27:57 +0530

    The pojo code for the patient reading code.

---
+++ Patient.java
@@ -0,0 +1,15 @@
+public class Patient{
+
+	private int reading = 0; 
+
+
+	public int getReading(){
+		return this.reading;
+	}
+
+	public void setReading(int patientreading){
+
+			this.reading = patientreading;
+		
+	}
+}
