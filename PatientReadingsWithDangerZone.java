2746b5473463dc70b897d5e21cc65847002bc2a1
Srihita Sai Ayinala <58342544+srihita30@users.noreply.github.com>
2020-05-28 23:11:32 +0530

    This code will set "danger zone" for the patients whose variance is more than 10 by taking in different readings for different users and calculating the percentage change.

---
+++ PatientReadingsWithDangerZone.java
@@ -0,0 +1,161 @@
+import java.util.Scanner;
+
+import java.util.ArrayList;
+
+//This code will take in different readings for different users and calculate the percentage change and set dangerzone for the patients whose variance is more than 10. 
+
+public class PatientReadingsWithDangerZone {
+	
+	public static void main(String[] args){
+
+		Scanner scannerObj = new Scanner(System.in);
+
+		//Number of patients. 
+
+		System.out.println("Enter the number of patients: ");
+
+		int patientNumber = scannerObj.nextInt();
+		
+		System.out.println("Enter the number of readings: ");
+		
+		int patientReadings = scannerObj.nextInt();
+
+		int[] nameArray = new int[patientNumber]; //Array is used only for the outer "for loop" sake - length purpose. 
+		
+		int[] readingArray = new int[patientReadings]; //Array is used only for the inner "for loop" sake - length purpose. 
+
+		ArrayList<PatientDangerPojo> patientArray = new ArrayList<PatientDangerPojo>();
+
+			//Loop to take in all of the patient name and reading. 
+
+			for(int i = 0; i < nameArray.length; i++)
+
+			{
+
+				scannerObj.nextLine(); 
+				
+				System.out.println("Enter the patient name: ");
+				
+				String name = scannerObj.nextLine();
+				
+				PatientDangerPojo patientNameAndReading = new PatientDangerPojo();
+				
+				patientNameAndReading.setThePatient(name); 
+				
+				ArrayList<Double> arrayOfReadings = new ArrayList<Double>();
+
+				
+					for(int y = 0; y<readingArray.length; y++) 
+
+					{
+
+						//System.out.println("The length of the reading array is " + readingArray.length); 
+
+						System.out.println("Enter the patient's reading: "); 
+
+						int theReading = scannerObj.nextInt();
+				
+						arrayOfReadings.add((double) theReading);
+
+						//System.out.println("The patient's reading is at this value of the index: " + readingArray[y]); 
+	  			
+					}
+				
+				patientNameAndReading.setReadings(arrayOfReadings); 
+				
+				patientArray.add(patientNameAndReading); 
+				
+				}
+
+			PatientReadingsWithDangerZone patientObj = new PatientReadingsWithDangerZone();
+
+			patientObj.thePatientReading(patientArray); 
+			patientObj.theDangerPatient(patientArray); 
+	}
+
+	//This code is the logic behind the percentage change. 
+
+	public void thePatientReading(ArrayList<PatientDangerPojo> arrayOfPatients){
+
+		/*
+
+		The formula for the percentage change i.e. percentage change = ((final-initial)/initial) * 100 is divided into three parts : the difference, the division part, and the percentage part. 
+
+		The final value i.e. the percentage change value is then set and printed. 
+
+		*/
+
+		double diffOfReadings = 0;
+		double varianceOfReadings = 0;
+		double percentageChange = 0; 
+
+		for(int x=0; x<arrayOfPatients.size(); x++ ){
+
+			PatientDangerPojo po = arrayOfPatients.get(x);
+
+			ArrayList<Double> percentageDifference = new ArrayList<Double>();
+
+			System.out.println("The name of the patient is: " + po.getThePatient()); 
+
+				for(int a = 0; a<po.getReadings().size() - 1 ; a++){
+
+					diffOfReadings = po.getReadings().get(a+1) - po.getReadings().get(a);
+
+					varianceOfReadings = diffOfReadings/po.getReadings().get(a);
+
+					percentageChange = varianceOfReadings * 100; 
+
+					percentageDifference.add(percentageChange);
+
+					po.setFinalValue(percentageDifference); 
+
+				System.out.println("And this patient's final reading after the percentage change has been calculated is " + percentageChange); 
+
+				}
+
+		}
+
+	}
+
+	//This method will set the dangerzone for the patients depending on the variance and whether or not it is greater than 10. 
+
+	public void theDangerPatient(ArrayList<PatientDangerPojo> dangerPatientArray){
+
+		String dangerColour = "red"; 
+
+		for(int s = 0; s<dangerPatientArray.size(); s++){
+
+		PatientDangerPojo dp = dangerPatientArray.get(s); 
+
+		//is conditions to determine the dangerzone. 
+
+			if(dp.getFinalValue().size() >= 2)
+
+			{
+
+			if(dp.getFinalValue().get(s)>= 10)
+
+				{
+					if(dp.getFinalValue().get(s+1) >= 10)
+
+						{
+							dp.setDangerZone(dangerColour); 
+						}
+				}
+
+				if(dp.getFinalValue().get(s)>= (-10))
+
+				{
+					if(dp.getFinalValue().get(s+1) >= (-10))
+
+						{
+							dp.setDangerZone(dangerColour); 
+						}
+				}
+
+			}
+		}
+
+	}
+
+}
